afd49e50e212df767b4a0a007ae4e305172eb92b
DragonflyStats <kobriendublin@gmail.com>
2018-05-14 14:41:30 +0100

    Create BreastCancer-4.R

---
+++ Keras/BreastCancer-4.R
@@ -0,0 +1,10 @@
+pre-cleaned BC data
+
+
+
+# Create a model with 5 hidden units
+# A dense, output layer using the "sigmoid" activation function
+
+# Compile the model using the "binary_crossentropy" as the loss function
+
+# fit the model over 20 epochs
